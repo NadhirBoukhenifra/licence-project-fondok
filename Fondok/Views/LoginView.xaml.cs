4251ac931801788010127966b1a4596f5b62471b
Nadhir Boukhenifra <nadhirboukhenifra@hotmail.com>
2017-05-27 12:51:35 +0100

    Add :package: SQLite.

--- Fondok/Views/LoginView.xaml.cs
+++ Fondok/Views/LoginView.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Data;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
@@ -31,12 +32,49 @@ namespace Fondok.Views
 
         private void Login_Button_Click(object sender, RoutedEventArgs e)
         {
-            Globals.isLogin = !Globals.isLogin;
+            //Globals.isLogin = !Globals.isLogin;        
             
-            Application.Current.MainWindow.Show();
-            
-            this.Hide();
-            
+            SQLiteDatabase DB = new SQLiteDatabase();
+
+            //Data Table login that Have All Queries From DB
+            DataTable Login;
+
+            //Query to Get Username & Password and Compare them In Db if Found!
+            String Query = "select * from USERS where USERNAME='" + this.userNameBox.Text + "' and PASSWORD='" + this.userPasswordBox.Password + "'";
+
+            //Put this information from DB to DataTable = login;
+            Login = DB.GetDataTable(Query);
+
+            //select grade,  username, password
+            string Grade = "select GRADE from USERS where USERNAME='" + this.userNameBox.Text + "' ";
+            string User = "select USERNAME from USERS where USERNAME='" + this.userNameBox.Text + "' ";
+            string Pass = "select PASSWORD from USERS where PASSWORD='" + this.userPasswordBox.Password + "' ";
+
+            //Assign (grade, username, password) to local variables (g, u, p)
+            string _Grade = DB.ExecuteScalar(Grade);
+            string _UserName = DB.ExecuteScalar(User);
+            string _Password = DB.ExecuteScalar(Pass);
+
+            if (this.userNameBox.Text == _UserName)
+            {
+
+                foreach (DataRow r in Login.Rows)
+                {
+
+                    if (_Grade == "admin")
+                    {
+                        MessageBox.Show(String.Format("Welcome {0} : {1}", _Grade, _UserName));
+                        Application.Current.MainWindow.Show();                    
+                        this.Hide();                 
+                    }
+                    else
+                    {
+                        MessageBox.Show(String.Format("Welcome {0} : {1}", _Grade, _UserName));
+                        Application.Current.MainWindow.Show();
+                        this.Hide();
+                    }
+                }
+            }            
         }
 
         private void Window_Closing(object sender, CancelEventArgs e)
