9c5d99c81d8edece3061811f3d4cf1463a75c80b
Simon Probook <Simon Probook@LITE>
2017-06-10 05:17:27 +0100

    hola amino xDDD

--- Fondok/Views/Windows/ClientWindow.xaml.cs
+++ Fondok/Views/Windows/ClientWindow.xaml.cs
@@ -103,15 +103,18 @@ namespace Fondok.Views.Windows
         private void ClientFirstNameField_PreviewKeyDown(object sender, KeyEventArgs e)
         {
 
-            if (e.Key >= Key.A && e.Key <= Key.Z)
-            {
-            }
-            else
+            //if (e.Key >= Key.A && e.Key <= Key.Z)
+            //{
+            //}
+            //else
+            //{
+            //    e.Handled = true;
+            //}
+
+            if ((e.Key >= Key.NumPad0 && e.Key <= Key.NumPad9) || (e.Key >= Key.D0 && e.Key <= Key.D9))
             {
                 e.Handled = true;
-            }
-
-
+            } //hola hola! xD 
 
         }
 
